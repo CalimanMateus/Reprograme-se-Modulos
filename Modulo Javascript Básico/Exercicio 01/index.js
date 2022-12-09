b9de9d123c1f3defe1065617210836030244cccd
CalimanMateus <mateus-caliman@hotmail.com>
2022-12-09 14:28:00 -0300

    Atualização 09/12

---
+++ Modulo Javascript Básico/Exercicio 01/index.js
@@ -0,0 +1,13 @@
+
+let n1 = parseInt(prompt("Digite o primeiro número: "))
+let n2 = parseInt(prompt("Digite o segundo número: "))
+let n3 = parseInt(prompt("Digite o terceiro número: "))
+
+soma = n1 + n2 + n3 
+media = ((n1+n2+n3)/3)
+
+alert (
+    "Resultados:" +
+    "\nSoma dos três números: " + soma +
+    "\nMédia dos três números: " + media
+)
