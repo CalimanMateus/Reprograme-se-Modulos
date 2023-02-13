96447683215a901626307a67661503884703eb13
Mateus Caliman Araujo <100041556+CalimanMateus@users.noreply.github.com>
2023-02-13 15:39:04 -0300

    att semana 3

--- Javascript DOM/Semana 3/Exercicio da semana/script.js
+++ Javascript DOM/Semana 3/Exercicio da semana/script.js
@@ -21,11 +21,11 @@
 
 
 function getLocalStorage() {
-  return JSON.parse(localStorage.getItem('bd_clientes')) ?? [];
+  return JSON.parse(localStorage.getItem('bd_aeronaves')) ?? [];
 }
 
-function setLocalStorage(bd_clientes) {
-  localStorage.setItem('bd_clientes', JSON.stringify(bd_clientes));
+function setLocalStorage(bd_aeronaves) {
+  localStorage.setItem('bd_aeronaves', JSON.stringify(bd_aeronaves));
 }
 
 function limparTabela() {
@@ -37,16 +37,21 @@ function limparTabela() {
 
 function atualizarTabela() { // Adaptação da função atualizarTabela (5 pontos)
   limparTabela();
-  const bd_clientes = getLocalStorage();
+  const bd_aeronaves = getLocalStorage();
   let index = 0;
-  for (cliente of bd_clientes) {
+  for ( aeronave of bd_aeronaves) {
     const novaLinha = document.createElement('tr');
+    if (aeronave.passageiros === 0) {
+      novaLinha.classList.add('table-danger');
+    }
     novaLinha.innerHTML = `
-        <th scope="row">${index}</th>
-        <td>${cliente.nome}</td>
-        <td>${cliente.email}</td>
-        <td>${cliente.celular}</td>
-        <td>${cliente.estado}</td>
+        <th scope="row">${index + 1}</th>
+        <td>${aeronave.codigoAeronave}</td>
+        <td>${aeronave.nomeComandante}</td>
+        <td>${aeronave.passageiros}</td>
+        <td>${aeronave.cargaMin}</td>
+        <td>${aeronave.cargaMax}</td>
+        <td>${aeronave.classeAeronave}</td>
         <td>
             <button type="button" class="btn btn-danger" id="${index}" onclick="excluir(${index})">Excluir</button>
         </td>
@@ -57,35 +62,37 @@ function atualizarTabela() { // Adaptação da função atualizarTabela (5 ponto
 }
 
 function inserir() { // Adaptação da função inserir (10 pontos)
-  const cliente = {
-    nome: document.getElementById('nome').value,
-    email: document.getElementById('email').value,
-    celular: document.getElementById('celular').value,
-    estado: document.getElementById('estado').value
+  const aeronave = {
+    codigoAeronave: document.getElementById('codigoAeronave').value,
+    nomeComandante: document.getElementById('nomeComandante').value,
+    passageiros: document.getElementById('passageiros').value,
+    cargaMin: document.getElementById('cargaMin').value,
+    cargaMax: document.getElementById('cargaMax').value,
+    classeAeronave: document.getElementById('ClasseAeronave').value
   }
-  const bd_clientes = getLocalStorage();
-  bd_clientes.push(cliente);
-  setLocalStorage(bd_clientes);
+  const bd_aeronaves = getLocalStorage();
+  bd_aeronaves.push(aeronave);
+  setLocalStorage(bd_aeronaves);
   atualizarTabela();
 }
 
 function excluir(index) { // Adaptação da função excluir (5 pontos)
-  const bd_clientes = getLocalStorage();
-  bd_clientes.splice(index, 1);
-  setLocalStorage(bd_clientes);
+  const bd_aeronaves = getLocalStorage();
+  bd_aeronaves.splice(index, 1);
+  setLocalStorage(bd_aeronaves);
   atualizarTabela();
 }
 
-function validarCelular() { // Adaptação da função validar (10 pontos)
-  const bd_clientes = getLocalStorage();
-  for (cliente of bd_clientes) {
-    if (celular.value == cliente.celular) {
-      celular.setCustomValidity("Este número de celular já existe!");
-      feedbackCelular.innerText = "Este número de celular já existe!";
+function validarCodigoAeronave() { // Adaptação da função validar (10 pontos)
+  const bd_aeronaves = getLocalStorage();
+  for (aeronave of bd_aeronaves) {
+    if (codigoAeronave.value == aeronave.codigoAeronave) {
+      codigoAeronave.setCustomValidity("Este Código de Aeronave já existe!");
+      feedbackCodigo.innerText = "Este Código de Aeronave já existe!";
       return false;
     } else {
-      celular.setCustomValidity("");
-      feedbackCelular.innerText = "Informe o celular corretamente.";
+      codigoAeronave.setCustomValidity("");
+      feedbackCodigo.innerText = "Informe o Códiogo da Aeronave Corretamente.";
     }
   }
   return true;
@@ -93,6 +100,6 @@ function validarCelular() { // Adaptação da função validar (10 pontos)
 
 atualizarTabela();
 // Seleção dos elementos e adição do listener para validação customizada (5 pontos)
-const celular = document.getElementById("celular");
-const feedbackCelular = document.getElementById("feedbackCelular");
-celular.addEventListener('input', validarCelular);
+const codigoAeronave = document.getElementById("codigoAeronave");
+const feedbackCodigo = document.getElementById("feedbackCodigo");
+codigoAeronave.addEventListener('input', validarCodigoAeronave);
